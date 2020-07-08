2af72479d5b3d803766feb2dd5eb4f3c7a55dee2
abre-lata <36392912+abre-lata@users.noreply.github.com>
2020-07-09 03:53:51 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Brand from "./Brand";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Brand />
+    </div>
+  );
+}
